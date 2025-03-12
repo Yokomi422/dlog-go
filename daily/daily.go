0ab04ce272d02ffbba2cb85c24724478cd791907
Rei Tsukada <rayzou1120@icloud.com>
2025-03-12 13:06:17 +0900

    more refac about the previous regexp refac

--- daily/daily.go
+++ daily/daily.go
@@ -1,7 +1,6 @@
 package daily
 
 import (
-	"fmt"
 	"io/fs"
 	"regexp"
 	"strconv"
@@ -13,57 +12,49 @@ type Daily struct {
 	Data []byte
 }
 
-// Filter returns Daily slice filtered by year and month
-// year and month is optional, which means no year returns dailies which have
-// the assigned month. If you want to get all dailies, you should set year and month
-// to be -1.
-func (d Daily) Filter(fileSystem fs.FS, year, month int) ([]Daily, error) {
-	filteredDailies := make([]Daily, 0)
-	dailies, err := fs.ReadDir(fileSystem, ".")
+// FetchSpecifiedDailies retrieves Daily slices from the given fileSystem.
+// If year, month, or day is -1, they will be treated as "not specified".
+// - year = -1: defaults to current year
+// - month = -1: defaults to current month
+// - day = -1: means "do not filter by day" (i.e. get the entire month)
+func (d Daily) FetchSpecifiedDailies(fileSystem fs.FS, year, month, day int) ([]Daily, error) {
+	now := time.Now()
+	if year == -1 {
+		year = now.Year()
+	}
+	if month == -1 {
+		month = int(now.Month())
+	}
+
+	entries, err := fs.ReadDir(fileSystem, ".")
 
 	if err != nil {
 		return nil, err
 	}
-	r, err := regexp.Compile("(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2}).md")
-	if err != nil {
-		return nil, err
-	}
-	// pattern generate
-	if year == -1 && month == -1 {
-		r, err = regexp.Compile("(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2}).md")
-		if err != nil {
-			return nil, err
-		}
-	} else if year == -1 {
-		r, err = regexp.Compile("(?P<year>[0-9]{4})-" + fmt.Sprintf("%02d", month) + "-[0-9]{2}.md")
-		if err != nil {
-			return nil, err
-		}
-	} else if month == -1 {
-		r, err = regexp.Compile(strconv.Itoa(year) + "-[0-9]{2}-[0-9]{2}.md")
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		r, err = regexp.Compile(strconv.Itoa(year) + "-" + fmt.Sprintf("%02d", month) + "-[0-9]{2}.md")
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	for _, d := range dailies {
-		matched := r.MatchString(d.Name())
-		if !matched {
+	filtered := make([]Daily, 0)
+	var re = regexp.MustCompile(`([0-9]{4})-([0-9]{2})-([0-9]{2}).md`)
+	for _, d := range entries {
+		name := d.Name()
+		if !re.MatchString(name) {
 			continue
 		}
-		data, _ := fs.ReadFile(fileSystem, d.Name())
-		filteredDailies = append(filteredDailies, Daily{Path: d.Name(), Data: data})
-	}
-	return filteredDailies, nil
-}
+		match := re.FindStringSubmatch(name)
+		fileYear, _ := strconv.Atoi(match[1])
+		fileMonth, _ := strconv.Atoi(match[2])
+		fileDay, _ := strconv.Atoi(match[3])
 
-func (d Daily) currentDate() (year, month, day int) {
-	year, _, day = time.Now().Date()
-	month = int(time.Now().Month())
-	return
+		if year != fileYear || month != fileMonth {
+			continue
+		}
+		if day != -1 && day != fileDay {
+			continue
+		}
+
+		data, err := fs.ReadFile(fileSystem, d.Name())
+		if err != nil {
+			return nil, err
+		}
+		filtered = append(filtered, Daily{Path: d.Name(), Data: data})
+	}
+	return filtered, nil
 }
--- daily/daily_test.go
+++ daily/daily_test.go
@@ -15,26 +15,13 @@ func TestDaily(t *testing.T) {
 		"2025-01-03.md": {Data: []byte("test")},
 		"2024-03-04.md": {Data: []byte("test")},
 	}
-	t.Run("Filter by year", func(t *testing.T) {
-		fetchedDailies, err := dailies.Filter(vfs, 2024, -1)
-
-		if err != nil {
-			t.Fatal(err)
-		}
-		expected := []daily.Daily{
-			daily.Daily{Path: "2024-01-01.md", Data: []byte("test")},
-			daily.Daily{Path: "2024-01-02.md", Data: []byte("test")},
-			daily.Daily{Path: "2024-03-04.md", Data: []byte("test")},
-		}
-		assertFilteredDailies(t, fetchedDailies, expected)
-	})
 	t.Run("Filter by year and month", func(t *testing.T) {
-		fetchedDailies, err := dailies.Filter(vfs, 2024, 3)
+		fetchedDailies, err := dailies.FetchSpecifiedDailies(vfs, 2024, 3, -1)
 		if err != nil {
 			t.Fatal(err)
 		}
 		expected := []daily.Daily{
-			daily.Daily{Path: "2024-03-04.md", Data: []byte("test")},
+			{Path: "2024-03-04.md", Data: []byte("test")},
 		}
 		assertFilteredDailies(t, fetchedDailies, expected)
 	})
